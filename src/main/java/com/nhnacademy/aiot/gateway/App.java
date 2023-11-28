8065ba558aab45dbcbb87d778a476d1cf52c1611
HyeonSeong9 <a01062704312@gmail.com>
2023-11-28 11:07:03 +0900

    gogo

--- src/main/java/com/nhnacademy/aiot/gateway/App.java
+++ src/main/java/com/nhnacademy/aiot/gateway/App.java
@@ -4,10 +4,11 @@ package com.nhnacademy.aiot.gateway;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+        System.out.println("Hello World!");
     }
 }
