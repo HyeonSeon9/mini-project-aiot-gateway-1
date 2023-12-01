46621a01cfec13da8ecc1b3bb22c0682c83bdad2
july433 <july433@naver.com>
2023-12-01 15:41:11 +0900

    사용하지 않는 임포트 수정

--- src/test/java/com/nhnacademy/aiot/WireCountTest.java
+++ src/test/java/com/nhnacademy/aiot/WireCountTest.java
@@ -2,11 +2,8 @@ package com.nhnacademy.aiot;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import com.nhnacademy.aiot.message.Message;
 import com.nhnacademy.aiot.node.SplitNode;
-import com.nhnacademy.aiot.wire.BufferedWire;
-import com.nhnacademy.aiot.wire.Wire;
+
 
 class WireCountTest {
     SplitNode splitNode = new SplitNode("SplitNode", 5);
